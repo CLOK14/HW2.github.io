a255813a3c4c8f92cb3140c25753e4fb1a837ed0
CLOK14 <67423670+CLOK14@users.noreply.github.com>
2021-12-01 00:31:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// let f = prompt("Please give me you money!!!")
+//
+// function hay(x) {
+//     alert(x)
+// }
+//
+// hay(f + "\n Hello world")
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
